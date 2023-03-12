2bb85afe5faf4a82f06f28f765273cf73e2cc273
SeanMcAuliffe <mcauliffesean@proton.me>
2023-03-11 16:08:26 -0800

    Add down-sampling step to dataset download script

--- dataset_download.sh
+++ dataset_download.sh
@@ -4,29 +4,27 @@
 # this can be done by running the ./setup.sh script (on debian based machines)
 # or following the instructions at https://cloud.google.com/sdk/docs/install#deb
 
+echo ""
+echo "--- Landsat Imageset Download ---"
+echo "Note: This script can take a long time to run"
+echo ""
+
 function usage {
-  echo "Usage: ./dataset_download.sh [-d output_directory] [-b|--band data_band] [-h|--help]"
+  echo "Usage: ./dataset_download.sh [-d output_directory] [-b data_band] [-s <WidthxHeight>] [-h]"
   echo "Options:"
-  echo "  -h                    Show this help message and exit"
-  echo "  -b --band <band>      Specify the EM band to use, default is B7: https://www.usgs.gov/faqs/what-are-best-landsat-spectral-bands-use-my-research"
-  echo "  -d --directory <dir>  Specify the output directory, default is ./data/images"
+  echo "  -h                  Show this help message and exit"
+  echo "  -b <band>           Specify the EM band to use, default is B7: https://www.usgs.gov/faqs/what-are-best-landsat-spectral-bands-use-my-research"
+  echo "  -s <WidthxHeight>   Specify the output image size, default is 512x512"
 }
 
-while getopts ":d:b:h-:" opt; do
+while getopts ":s:b:h-:" opt; do
   case ${opt} in
-    d ) output_directory="$OPTARG"
+    b ) IMGBAND="$OPTARG"
       ;;
-    b|--band ) IMGBAND="$OPTARG"
+    s ) SIZE="$OPTARG"
       ;;
     h) usage
       exit 0
-      ;;
-    - ) case "${OPTARG}" in
-          band=* ) IMGBAND="${OPTARG#*=}"
-            ;;
-          directory=* ) output_directory="${OPTARG#*=}"
-            ;;
-        esac
       ;;
     \? ) echo "Invalid option: -$OPTARG" 1>&2
       exit 1
@@ -38,19 +36,62 @@ while getopts ":d:b:h-:" opt; do
 done
 shift $((OPTIND -1))
 
+
+trap ctrl_c INT
+function ctrl_c() {
+  echo ""
+  echo "Ending download, Total files downloaded: $files_download"
+  if [[ ! -z "$filename" ]]; then
+    convert "$IMAGE_DIRECTORY/$filename" -sample "$SIZE" "$IMAGE_DIRECTORY/$filename" 2>/dev/null
+  fi
+  exit 1
+}
+
+
 if [ -z "$IMGBAND" ] ; then
   IMGBAND="B7"
 fi
 
-if [ -z "$output_directory" ] ; then
-  output_directory="./data/images"
+if [ -z "$SIZE" ] ; then
+  SIZE="512x512"
 fi
 
-# Download landsat 4 images ending in _"$band".TIF
-files_download=0
-value="001"
 
-for i in {1..40}
+BATCH_NAME="${IMGBAND}_${SIZE}"
+IMAGE_DIRECTORY="./data/images/$BATCH_NAME"
+METADATA_DIRECTORY="./data/metadata/$BATCH_NAME"
+
+
+if [ ! -e "./data/images" ]; then
+  mkdir -p "./data/images"
+fi
+
+if [ ! -e "./data/metadata" ]; then
+  mkdir -p "./data/metadata"
+fi
+
+if [ ! -e "./data/images/$BATCH_NAME" ]; then
+  mkdir -p "./data/images/$BATCH_NAME"
+fi
+
+if [ ! -e "./data/metadata/$BATCH_NAME" ]; then
+  mkdir -p "./data/metadata/$BATCH_NAME"
+fi
+
+
+echo "Band: $IMGBAND"
+echo "Size: $SIZE"
+echo "Batch Name: $BATCH_NAME"
+echo "Image Directory: $IMAGE_DIRECTORY"
+echo "Metadata Directory: $METADATA_DIRECTORY"
+echo ""
+
+
+filename=""
+files_download=0
+value="180"
+
+for i in {180..230}
 do
   #Format the string with the incremented substring
   echo "Downloading images for row gs://gcp-public-data-landsat/LT04/01/$value/"
@@ -80,33 +121,29 @@ do
 
       for element in "${basearray[@]}"
         do
-          # Check if the element ends with the string "_B7.tif"
+          # Check if the element ends with the string "_<BAND>.tif"
           if [[ "$element" == *_"$IMGBAND".TIF ]]; then
-            # Check if the file has already been downloaded
-            if [ -f "$output_directory/$(basename $element)" ]; then
-              continue
-            else
-              gsutil -m cp "$element" "$output_directory"
-              files_download=$((files_download + 1))
-            fi
+            filename=$(basename $element)
+            gsutil -m cp -n "$element" "$IMAGE_DIRECTORY"
+            convert "$IMAGE_DIRECTORY/$filename" -sample "$SIZE" "$IMAGE_DIRECTORY/$filename" 2>/dev/null
+            files_download=$((files_download + 1))
           fi
-          # DOwnload the corresponding .txt file
+          # Download the corresponding .txt file
           if [[ "$element" == *MTL.txt ]]; then
-            # Check if the file has already been downloaded
-            if [ -f "$output_directory/$(basename $element)" ]; then
-              continue
-            else
-              gsutil -m cp "$element" ./data/metadata
-            fi
+            gsutil -m cp -n "$element" "$METADATA_DIRECTORY"
           fi
       done
-
   done
 
-  # Increment the value substring by 1
+  if [ $files_download -eq 6000 ]; then
+    echo "Ending download, Total files downloaded: $files_download"
+    exit 0
+  fi
+
   value=$(printf '%03d' $((10#$value + 1)))
 
 done
 
-# Report the number of files downloaded
-echo "Downloaded $files_download files"
+
+LT04_L1GS_182010_19880901_20170206_01_T2_B7
+
