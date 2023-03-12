a3b96a7772ba18039f6ea601b4662bfb062ed4be
Mateo <mateo.a.moody@gmail.com>
2023-03-11 21:41:28 -0800

    model now properly fits data

--- model.py
+++ model.py
@@ -17,14 +17,20 @@ for filename in os.listdir("./data/images"):
    image_resized = resize(im, (2150, 2205), anti_aliasing=True)
    if image_resized is not None:
       print(image_resized.shape)
-      np.append(X_train, image_resized)
+      X_train.append(image_resized)
 
-
+X_train = np.array(X_train)
+n, x, y = X_train.shape
+print(n, x, y)
+X_train = np.reshape(X_train, (n, x*y))
+print(X_train.shape)
 for x in range(0, 9):
    y_train.append(1)
 
-for x in range(0, 9):
+for x in range(0, 11):
    y_train.append(0)
+
+y_train = np.array(y_train)
 
 """
 for im in X_train:
@@ -34,3 +40,5 @@ for im in X_train:
 """
 clf = svm.SVC(random_state=0, kernel = "rbf")
 clf.fit(X_train, y_train)
+
+print(clf.score(X_train, y_train))
